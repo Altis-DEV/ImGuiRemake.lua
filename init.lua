1d79fbc9624b37edfffc0f03460d9d30c253680c
Altis <flandrescripter@gmail.com>
2026-08-11 21:07:49 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,23 +1,22 @@
--- File: ImGuiRemake/init.lua
+-- File: ImGuiRemake.lua/init.lua
 local ImGui = {}
-
 local repo = "https://raw.githubusercontent.com/Altis-DEV/ImGuiRemake.lua/refs/heads/main/"
 
 local Theme = loadstring(game:HttpGet(repo .. "Theme.lua"))()
 local WindowModule = loadstring(game:HttpGet(repo .. "Components/Window.lua"))()
 local TabModule = loadstring(game:HttpGet(repo .. "Components/Tab.lua"))()
+local ButtonModule = loadstring(game:HttpGet(repo .. "Components/Button.lua"))()
 
--- Inject TabModule vào WindowModule để Window có thể tạo tab
+-- Inject các Module phụ thuộc vào Window
 WindowModule._TabModule = TabModule
+WindowModule.ButtonModule = ButtonModule
+
+function ImGui:CreateWindow(options)
+    return WindowModule.new(options, Theme:GetTheme("Default"), Theme)
+end
 
 function ImGui:CreateTheme(name, colors)
     Theme:CreateTheme(name, colors)
-end
-
-function ImGui:CreateWindow(options)
-    local currentTheme = Theme:GetTheme("Default")
-    local newWindow = WindowModule.new(options, currentTheme, Theme)
-    return newWindow
 end
 
 return ImGui
