900e8ccacecf2f6726c37054bfb8941548650eb9
Altis <flandrescripter@gmail.com>
2026-08-11 21:34:05 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -6,10 +6,12 @@ local Theme = loadstring(game:HttpGet(repo .. "Theme.lua"))()
 local WindowModule = loadstring(game:HttpGet(repo .. "Components/Window.lua"))()
 local TabModule = loadstring(game:HttpGet(repo .. "Components/Tab.lua"))()
 local ButtonModule = loadstring(game:HttpGet(repo .. "Components/Button.lua"))()
+local ToggleModule = loadstring(game:HttpGet(repo .. "Components/Toggle.lua"))() -- [THÊM MỚI] Load Toggle.lua
 
 -- Inject các Module phụ thuộc vào Window
 WindowModule._TabModule = TabModule
 WindowModule.ButtonModule = ButtonModule
+WindowModule.ToggleModule = ToggleModule -- [THÊM MỚI] Inject ToggleModule vào Window
 
 function ImGui:CreateWindow(options)
     return WindowModule.new(options, Theme:GetTheme("Default"), Theme)
