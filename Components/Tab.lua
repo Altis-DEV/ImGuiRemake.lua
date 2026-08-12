27297d7fdac0f19a7fc079b13c1ba9f8c7a25341
Altis <flandrescripter@gmail.com>
2026-08-12 19:14:28 +0700

    Update Tab.lua

--- Components/Tab.lua
+++ Components/Tab.lua
@@ -227,6 +227,22 @@ function Tab:Image(options)
         options or {}
     )
 end
+
+function Tab:ColorPreview(options)
+    if not self.Window.ColorPreviewModule then
+        warn(
+            "ColorPreviewModule chưa được load " ..
+            "(Kiểm tra lại file init.lua)!"
+        )
+
+        return nil
+    end
+
+    return self.Window.ColorPreviewModule.new(
+        self,
+        options or {}
+    )
+end
 ----------------------------------------------------------------
 -- SELECT
 ----------------------------------------------------------------
